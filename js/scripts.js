81a0d868b880b722fb679cbefde75a8f1ab56d17
Aaron Whang <tlbhibiki@gmail.com>
2019-06-07 09:15:29 -0700

    it will add checkboxes and a way to store the number of checked boxes. There is a test alert to see that it works.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,85 @@
 // if program name is 0 it will clear the highlight of the row
 // each program name will reset to 0 after check and highlight, may just call the variable names = 0 within the on click function
 // this will run on checkbox click
+$(document).ready(function(){
+
+$("input").click(function(){
+  var collection = 0;
+  if ($("input:checkbox[name=vehicle1]:checked").val() === "1"){
+    collection += 1;
+  }
+
+  if ($("input:checkbox[name=vehicle2]:checked").val() === "1"){
+    collection += 1;
+  }
+
+  if ($("input:checkbox[name=vehicle3]:checked").val() === "1"){
+    collection += 1;
+  }
+
+  if ($("input:checkbox[name=vehicle4]:checked").val() === "1"){
+    collection += 1;
+  }
+
+  if ($("input:checkbox[name=vehicle5]:checked").val() === "1"){
+    collection += 1;
+  }
+
+  if ($("input:checkbox[name=vehicle6]:checked").val() === "1"){
+    collection += 1;
+  }
+  
+  alert(collection);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
