ede7603316507cc022fea1e035d38acb73b8e15c
Aaron Whang <tlbhibiki@gmail.com>
2019-06-07 08:54:25 -0700

    it will add comments to outline the design of the web page and the js variables data structure

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// 7 program names to collect value
+// each checkbox will add 1 to the value of associated program names
+// if program name is over 1 it will highlight the row
+// if program name is 0 it will clear the highlight of the row
+// each program name will reset to 0 after check and highlight, may just call the variable names = 0 within the on click function
+// this will run on checkbox click
